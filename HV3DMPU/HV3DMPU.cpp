6ed7cf4e8b396d115b246fa776f449f5cdae2b48
Rebeka Fox <rebekafox@outlook.com>
2020-08-19 01:03:33 -0700

    Initial commit

---
+++ HV3DMPU/HV3DMPU.cpp
@@ -0,0 +1,10 @@
+// HV3DMPU.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnHV3DMPU()
+{
+}
